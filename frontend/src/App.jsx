c51790203ef5380453421465b24ccb4f28c769c2
Shreya <shreyarai1503@gmail.com>
2024-10-08 22:54:05 +0530

    update app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Sidebar from "./components/sidebar/Sidebar";
+import Header from "./components/appBar/Header";
 
 function App() {
   return (
     <Router>
       <div className="app flex flex-col h-screen">
-        <Sidebar />
+        <Header />
+        <div className="flex flex-1">
+          <Sidebar />
+        </div>
       </div>
     </Router>
   );
